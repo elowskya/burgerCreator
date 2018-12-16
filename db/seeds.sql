ac1028d7be078863ef2d72ef7258e765d0f4167f
unknown <ashley.elowsky123@gmail.com>
2018-12-16 14:27:10 -0800

    schema updated

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--populating default database of burgers to choose from--
+
+INSERT INTO burgers (burger_name, devoured) values ('Hawaiian', 'true');
+INSERT INTO burgers (burger_name, devoured) values ('BBQ', 'false');
+INSERT INTO burgers (burger_name, devoured) values ('BLT', 'false');
